e3153c664e0f4b30b3e0ab8d74ae3ae0ec1ed233
findacodes7 <findacodes7@gmail.com>
2016-05-20 19:08:53 +0530

    Android Images Gallery

---
+++ AndroidGalleryView/gen/androidinterview/com/androidgalleryview/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package androidinterview.com.androidgalleryview;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
